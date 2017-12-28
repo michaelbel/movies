d01c010497a0c3956f31e78801531640542994a9
Michael Bel <michael-bel@outlook.com>
2017-12-29 00:04:21 +0300

    update fragment

--- app/src/main/java/org/michaelbel/application/ui/fragment/ReviewsMovieFragment.java
+++ app/src/main/java/org/michaelbel/application/ui/fragment/ReviewsMovieFragment.java
@@ -8,16 +8,21 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import org.michaelbel.application.R;
 import org.michaelbel.application.moviemade.ApiFactory;
+import org.michaelbel.application.moviemade.LayoutHelper;
 import org.michaelbel.application.moviemade.Theme;
 import org.michaelbel.application.moviemade.Url;
 import org.michaelbel.application.rest.api.MOVIES;
+import org.michaelbel.application.rest.model.Movie;
 import org.michaelbel.application.rest.model.Review;
 import org.michaelbel.application.rest.response.ReviewResponse;
 import org.michaelbel.application.ui.MovieActivity;
@@ -32,22 +37,21 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+@SuppressWarnings("all")
 public class ReviewsMovieFragment extends Fragment {
 
-    private static final String MOVIE_ID = "movie_id";
-
-    private int movieId;
+    private Movie currentMovie;
 
     private ReviewsAdapter adapter;
     private MovieActivity activity;
-    private ArrayList<Review> reviewList = new ArrayList<>();
+    private ArrayList<Review> reviewsList = new ArrayList<>();
 
     private TextView emptyView;
-    private SwipeRefreshLayout refreshLayout;
+    private SwipeRefreshLayout fragmentView;
 
-    public static ReviewsMovieFragment newInstance(int movieId) {
+    public static ReviewsMovieFragment newInstance(Movie movie) {
         Bundle args = new Bundle();
-        args.putInt(MOVIE_ID, movieId);
+        args.putSerializable("movie", movie);
 
         ReviewsMovieFragment fragment = new ReviewsMovieFragment();
         fragment.setArguments(args);
@@ -58,38 +62,46 @@ public class ReviewsMovieFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         activity = (MovieActivity) getActivity();
 
-        View fragmentView = inflater.inflate(R.layout.fragment_review, container, false);
+        fragmentView = new SwipeRefreshLayout(activity);
+        fragmentView.setRefreshing(reviewsList.isEmpty());
+        fragmentView.setColorSchemeResources(Theme.accentColor());
         fragmentView.setBackgroundColor(ContextCompat.getColor(activity, Theme.backgroundColor()));
-
-        emptyView = fragmentView.findViewById(R.id.empty_view);
-        emptyView.setTextColor(ContextCompat.getColor(activity, Theme.secondaryTextColor()));
-
-        refreshLayout = fragmentView.findViewById(R.id.swipe_refresh_layout);
-        refreshLayout.setColorSchemeResources(Theme.accentColor());
-        refreshLayout.setRefreshing(true);
-        refreshLayout.setOnRefreshListener(() -> {
+        fragmentView.setOnRefreshListener(() -> {
             if (NetworkUtils.getNetworkStatus() == NetworkUtils.TYPE_NOT_CONNECTED) {
                 onLoadError();
             } else {
-                if (reviewList.isEmpty()) {
-                    getReviews();
+                if (reviewsList.isEmpty()) {
+                    loadReviews();
                 } else {
-                    refreshLayout.setRefreshing(false);
+                    fragmentView.setRefreshing(false);
                 }
             }
         });
 
+        FrameLayout contentLayout = new FrameLayout(activity);
+        contentLayout.setLayoutParams(LayoutHelper.makeSwipeRefresh(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
+        fragmentView.addView(contentLayout);
+
+        emptyView = new TextView(activity);
+        emptyView.setGravity(Gravity.CENTER);
+        emptyView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+        emptyView.setTextColor(ContextCompat.getColor(activity, Theme.secondaryTextColor()));
+        emptyView.setLayoutParams(LayoutHelper.makeFrame(LayoutHelper.WRAP_CONTENT, LayoutHelper.WRAP_CONTENT, Gravity.CENTER, 24, 0, 24, 0));
+        contentLayout.addView(emptyView);
+
         adapter = new ReviewsAdapter();
 
-        RecyclerListView recyclerView = fragmentView.findViewById(R.id.recycler_view);
+        RecyclerListView recyclerView = new RecyclerListView(activity);
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
         recyclerView.setEmptyView(emptyView);
         recyclerView.setLayoutManager(new LinearLayoutManager(activity));
-        recyclerView.setOnItemClickListener((view, position) ->
-                activity.startReview(reviewList.get(position).id)
-        );
-
+        recyclerView.setLayoutParams(LayoutHelper.makeFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
+        recyclerView.setOnItemClickListener((view, position) -> {
+            Review review = reviewsList.get(position);
+            activity.startReview(review, currentMovie);
+        });
+        contentLayout.addView(recyclerView);
         return fragmentView;
     }
 
@@ -97,55 +109,53 @@ public class ReviewsMovieFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         if (getArguments() != null) {
-            movieId = getArguments().getInt(MOVIE_ID);
+            currentMovie = (Movie) getArguments().getSerializable("movie");
         }
 
         if (NetworkUtils.getNetworkStatus() == NetworkUtils.TYPE_NOT_CONNECTED) {
             onLoadError();
         } else {
-            getReviews();
+            loadReviews();
         }
     }
 
-    private void getReviews() {
+    private void loadReviews() {
         MOVIES service = ApiFactory.getRetrofit().create(MOVIES.class);
-        Call<ReviewResponse> call = service.getReviews(movieId, Url.TMDB_API_KEY, Url.en_US, 1);
+        Call<ReviewResponse> call = service.getReviews(currentMovie.id, Url.TMDB_API_KEY, Url.en_US, 1);
         call.enqueue(new Callback<ReviewResponse>() {
             @Override
-            public void onResponse(Call<ReviewResponse> call, Response<ReviewResponse> response) {
+            public void onResponse(@NonNull Call<ReviewResponse> call, @NonNull Response<ReviewResponse> response) {
                 if (response.isSuccessful()) {
-                    if (!reviewList.isEmpty()) {
-                        reviewList.clear();
+                    if (!reviewsList.isEmpty()) {
+                        reviewsList.clear();
                     }
 
-                    reviewList.addAll(response.body().reviewList);
+                    reviewsList.addAll(response.body().reviewList);
                     adapter.notifyDataSetChanged();
 
-                    if (reviewList.isEmpty()) {
+                    if (reviewsList.isEmpty()) {
                         emptyView.setText(R.string.NoReviews);
                     }
 
                     onLoadSuccessful();
                 } else {
-                    //FirebaseCrash.report(new Error("Server not found"));
                     onLoadError();
                 }
             }
 
             @Override
-            public void onFailure(Call<ReviewResponse> call, Throwable t) {
-                //FirebaseCrash.report(t);
+            public void onFailure(@NonNull Call<ReviewResponse> call, @NonNull Throwable t) {
                 onLoadError();
             }
         });
     }
 
     private void onLoadSuccessful() {
-        refreshLayout.setRefreshing(false);
+        fragmentView.setRefreshing(false);
     }
 
     private void onLoadError() {
-        refreshLayout.setRefreshing(false);
+        fragmentView.setRefreshing(false);
         emptyView.setText(R.string.NoConnection);
     }
 
@@ -158,17 +168,17 @@ public class ReviewsMovieFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
-            Review review = reviewList.get(position);
+            Review review = reviewsList.get(position);
 
             ReviewView view = (ReviewView) holder.itemView;
-            view.setAuthor(review.author);
-            view.setContent(review.content);
-            view.setDivider(position != reviewList.size() - 1);
+            view.setAuthor(review.author)
+                .setContent(review.content)
+                .setDivider(position != reviewsList.size() - 1);
         }
 
         @Override
         public int getItemCount() {
-            return reviewList != null ? reviewList.size() : 0;
+            return reviewsList != null ? reviewsList.size() : 0;
         }
     }
 }
