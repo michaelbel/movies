b0a5c7960fe50ff44a9cc69b1de8be0835633579
Michael Bel <michael-bel@outlook.com>
2018-01-04 03:25:45 +0300

    update model

---
+++ app/src/main/java/org/michaelbel/application/model/SearchItem.java
@@ -0,0 +1,9 @@
+package org.michaelbel.application.model;
+
+import io.realm.RealmObject;
+
+public class SearchItem extends RealmObject {
+
+    public String queryDate;
+    public String queryTitle;
+}
