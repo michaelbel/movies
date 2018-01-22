fc4ca8f2489e79fad248eda53f13810074af01e7
Michael Bel <michael-bel@outlook.com>
2018-01-22 13:16:10 +0300

    update rxjava adapter

--- app/src/main/java/org/michaelbel/moviemade/rest/ApiFactory.java
+++ app/src/main/java/org/michaelbel/moviemade/rest/ApiFactory.java
@@ -6,7 +6,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiFactory {
@@ -20,7 +20,7 @@ public class ApiFactory {
         return new Retrofit.Builder()
                 .baseUrl(TMDB_API_ENDPOINT)
                 .addConverterFactory(GsonConverterFactory.create(GSON))
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
     }
 
