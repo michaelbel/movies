7cf12b1f39f04cd55b117fdfde39207268756d8f
Michael Bel <michael-bel@outlook.com>
2018-01-23 12:46:10 +0300

    update presenter

--- app/src/main/java/org/michaelbel/moviemade/mvp/presenter/CastMoviePresenter.java
+++ app/src/main/java/org/michaelbel/moviemade/mvp/presenter/CastMoviePresenter.java
@@ -15,13 +15,16 @@ import org.michaelbel.moviemade.util.NetworkUtils;
 import java.util.ArrayList;
 import java.util.List;
 
-import io.reactivex.Observer;
+import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 @InjectViewState
 public class CastMoviePresenter extends MvpPresenter<MvpResultsView> {
+
+    private Disposable disposable;
 
     public void loadCredits(int movieId) {
         if (NetworkUtils.notConnected()) {
@@ -31,32 +34,28 @@ public class CastMoviePresenter extends MvpPresenter<MvpResultsView> {
 
         // todo То, что этот метод вызывается 2 раза нехорошо, нужно вызывать его 1 раз и затем только передавать данные
         MOVIES service = ApiFactory.createService(MOVIES.class);
-        service.getCredits(movieId, Url.TMDB_API_KEY)
-               .subscribeOn(Schedulers.io())
-               .observeOn(AndroidSchedulers.mainThread())
-               .subscribe(new Observer<CreditResponse>() {
-                   @Override
-                   public void onSubscribe(Disposable d) {
+        Observable<CreditResponse> observable = service.getCredits(movieId, Url.TMDB_API_KEY).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+        disposable = observable.subscribeWith(new DisposableObserver<CreditResponse>() {
+            @Override
+            public void onNext(CreditResponse response) {
+                List<TmdbObject> results = new ArrayList<>(response.casts);
+                getViewState().showResults(results);
+            }
 
-                   }
+            @Override
+            public void onError(Throwable e) {
+                getViewState().showError(EmptyViewMode.MODE_NO_PEOPLE);
+                e.printStackTrace();
+            }
 
-                   @Override
-                   public void onNext(CreditResponse response) {
-                       List<TmdbObject> results = new ArrayList<>();
-                       results.addAll(response.casts);
+            @Override
+            public void onComplete() {}
+        });
+    }
 
-                       getViewState().showResults(results);
-                   }
-
-                   @Override
-                   public void onError(Throwable e) {
-                       getViewState().showError(EmptyViewMode.MODE_NO_PEOPLE);
-                   }
-
-                   @Override
-                   public void onComplete() {
-
-                   }
-               });
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        disposable.dispose();
     }
 }
