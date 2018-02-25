b834a2583da03a586dc6d5b68ce64d644ed8a942
Michael Bel <michael-bel@outlook.com>
2018-02-25 10:52:36 +0300

    Update base extensions

---
+++ app/src/main/java/org/michaelbel/core/extensions/AndroidExtensions.java
@@ -0,0 +1,30 @@
+package org.michaelbel.core.extensions;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import org.michaelbel.moviemade.app.Moviemade;
+
+/**
+ * Date: Sun, Feb 25 2018
+ * Time: 10:15 MSK
+ *
+ * @author Michael Bel
+ */
+
+public class AndroidExtensions {
+
+    private static Context getContext() {
+        return Moviemade.AppContext;
+    }
+
+    public static void copyToClipboard(@NonNull CharSequence text) {
+        ClipboardManager clipboard = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clipData = ClipData.newPlainText(text, text);
+        if (clipboard != null) {
+            clipboard.setPrimaryClip(clipData);
+        }
+    }
+}
