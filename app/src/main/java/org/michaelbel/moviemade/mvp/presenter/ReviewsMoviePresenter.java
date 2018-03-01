e2060c28355e30999b362720eaabc3cd46b47e60
Michael Bel <michael-bel@outlook.com>
2018-03-01 12:44:29 +0300

    Add composite disposables

--- app/src/main/java/org/michaelbel/moviemade/mvp/presenter/ReviewsMoviePresenter.java
+++ app/src/main/java/org/michaelbel/moviemade/mvp/presenter/ReviewsMoviePresenter.java
@@ -6,7 +6,7 @@ import com.arellomobile.mvp.MvpPresenter;
 import org.michaelbel.moviemade.app.Url;
 import org.michaelbel.moviemade.app.annotation.EmptyViewMode;
 import org.michaelbel.moviemade.mvp.view.MvpResultsView;
-import org.michaelbel.moviemade.rest.ApiFactory2;
+import org.michaelbel.moviemade.rest.ApiFactory;
 import org.michaelbel.moviemade.rest.TmdbObject;
 import org.michaelbel.moviemade.rest.api.MOVIES;
 import org.michaelbel.moviemade.rest.response.ReviewResponse;
@@ -17,8 +17,9 @@ import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 
 @InjectViewState
 public class ReviewsMoviePresenter extends MvpPresenter<MvpResultsView> {
@@ -30,7 +31,7 @@ public class ReviewsMoviePresenter extends MvpPresenter<MvpResultsView> {
 
     private int movieId;
 
-    private Disposable disposable1, disposable2;
+    private final CompositeDisposable disposables = new CompositeDisposable();
 
     public void loadFirstPage(int movieId) {
         this.movieId = movieId;
@@ -50,9 +51,9 @@ public class ReviewsMoviePresenter extends MvpPresenter<MvpResultsView> {
         loading = false;
         loadingLocked = false;
 
-        MOVIES service = ApiFactory2.createService(MOVIES.class);
-        Observable<ReviewResponse> observable = service.getReviews(movieId, Url.TMDB_API_KEY, Url.en_US, page).observeOn(AndroidSchedulers.mainThread());
-        disposable1 = observable.subscribeWith(new DisposableObserver<ReviewResponse>() {
+        MOVIES service = ApiFactory.createService2(MOVIES.class);
+        Observable<ReviewResponse> observable = service.getReviews(movieId, Url.TMDB_API_KEY, Url.en_US, page).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+        disposables.add(observable.subscribeWith(new DisposableObserver<ReviewResponse>() {
             @Override
             public void onNext(ReviewResponse response) {
                 List<TmdbObject> results = new ArrayList<>(response.reviews);
@@ -68,20 +69,17 @@ public class ReviewsMoviePresenter extends MvpPresenter<MvpResultsView> {
             @Override
             public void onError(Throwable e) {
                 getViewState().showError(EmptyViewMode.MODE_NO_REVIEWS);
-                e.printStackTrace();
             }
 
             @Override
-            public void onComplete() {
-                dispose();
-            }
-        });
+            public void onComplete() {}
+        }));
     }
 
     public void loadNextPage() {
-        MOVIES service = ApiFactory2.createService(MOVIES.class);
-        Observable<ReviewResponse> observable = service.getReviews(movieId, Url.TMDB_API_KEY, Url.en_US, page).observeOn(AndroidSchedulers.mainThread());
-        disposable2 = observable.subscribeWith(new DisposableObserver<ReviewResponse>() {
+        MOVIES service = ApiFactory.createService2(MOVIES.class);
+        Observable<ReviewResponse> observable = service.getReviews(movieId, Url.TMDB_API_KEY, Url.en_US, page).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+        disposables.add(observable.subscribeWith(new DisposableObserver<ReviewResponse>() {
             @Override
             public void onNext(ReviewResponse response) {
                 List<TmdbObject> results = new ArrayList<>(response.reviews);
@@ -94,28 +92,17 @@ public class ReviewsMoviePresenter extends MvpPresenter<MvpResultsView> {
             public void onError(Throwable e) {
                 loadingLocked = true;
                 loading = false;
-                e.printStackTrace();
             }
 
             @Override
-            public void onComplete() {
-                //disposable1.dispose();
-                dispose();
-            }
-        });
+            public void onComplete() {}
+        }));
         loading = true;
     }
 
     @Override
     public void onDestroy() {
+        disposables.dispose();
         super.onDestroy();
-
-        if (disposable1 != null && !disposable1.isDisposed()) {
-            disposable1.dispose();
-        }
-
-        if (disposable2 != null && !disposable2.isDisposed()) {
-            disposable2.dispose();
-        }
     }
 }
