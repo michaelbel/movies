85b0f8be25da3462051ec23c2fc8fbe69ead5f9a
Michael Bel <michael-bel@outlook.com>
2018-03-01 12:39:09 +0300

    Add composite disposables

--- app/src/main/java/org/michaelbel/moviemade/mvp/presenter/GenresPresenter.java
+++ app/src/main/java/org/michaelbel/moviemade/mvp/presenter/GenresPresenter.java
@@ -6,7 +6,7 @@ import com.arellomobile.mvp.MvpPresenter;
 import org.michaelbel.moviemade.app.Url;
 import org.michaelbel.moviemade.app.annotation.EmptyViewMode;
 import org.michaelbel.moviemade.mvp.view.MvpResultsView;
-import org.michaelbel.moviemade.rest.ApiFactory2;
+import org.michaelbel.moviemade.rest.ApiFactory;
 import org.michaelbel.moviemade.rest.TmdbObject;
 import org.michaelbel.moviemade.rest.api.service.GENRES;
 import org.michaelbel.moviemade.rest.response.GenresResponse;
@@ -17,13 +17,14 @@ import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 
 @InjectViewState
 public class GenresPresenter extends MvpPresenter<MvpResultsView> {
 
-    private Disposable disposable;
+    private final CompositeDisposable disposables = new CompositeDisposable();
 
     public void loadGenres() {
         if (NetworkUtils.notConnected()) {
@@ -31,9 +32,9 @@ public class GenresPresenter extends MvpPresenter<MvpResultsView> {
             return;
         }
 
-        GENRES service = ApiFactory2.createService(GENRES.class);
-        Observable<GenresResponse> observable = service.getMovieList(Url.TMDB_API_KEY, Url.en_US).observeOn(AndroidSchedulers.mainThread());
-        disposable = observable.subscribeWith(new DisposableObserver<GenresResponse>() {
+        GENRES service = ApiFactory.getRetrofit2().create(GENRES.class);
+        Observable<GenresResponse> observable = service.getMovieList(Url.TMDB_API_KEY, Url.en_US).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+        disposables.add(observable.subscribeWith(new DisposableObserver<GenresResponse>() {
             @Override
             public void onNext(GenresResponse response) {
                 List<TmdbObject> results = new ArrayList<>(response.genres);
@@ -43,22 +44,16 @@ public class GenresPresenter extends MvpPresenter<MvpResultsView> {
             @Override
             public void onError(Throwable e) {
                 getViewState().showError(EmptyViewMode.MODE_NO_CONNECTION);
-                e.printStackTrace();
             }
 
             @Override
-            public void onComplete() {
-                dispose();
-            }
-        });
+            public void onComplete() {}
+        }));
     }
 
     @Override
     public void onDestroy() {
+        disposables.dispose();
         super.onDestroy();
-
-        if (disposable != null && !disposable.isDisposed()) {
-            disposable.dispose();
-        }
     }
 }
