ba0baf5a67bb116b5d294984d16d53e05210682f
Michael Bel <michael-bel@outlook.com>
2018-03-04 19:14:31 +0300

    Update Extensions

--- app/src/main/java/org/michaelbel/core/extensions/Extensions.java
+++ app/src/main/java/org/michaelbel/core/extensions/Extensions.java
@@ -1,23 +1,14 @@
 package org.michaelbel.core.extensions;
 
-import android.annotation.TargetApi;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
-import android.support.annotation.ColorInt;
-import android.support.annotation.FloatRange;
 import android.support.annotation.NonNull;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import org.michaelbel.moviemade.R;
 import org.michaelbel.moviemade.app.Moviemade;
-
-import java.lang.reflect.Field;
 
 /**
  * Date: Sun, Feb 25 2018
@@ -38,102 +29,6 @@ public class Extensions {
         ClipData clipData = ClipData.newPlainText(text, text);
         if (clipboard != null) {
             clipboard.setPrimaryClip(clipData);
-        }
-    }
-
-    @ColorInt
-    public static int adjustAlpha(@ColorInt int color, @FloatRange(from = 0.00F, to = 1.00F) float factor) {
-        int alpha = Math.round(Color.alpha(color) * factor);
-        int red = Color.red(color);
-        int green = Color.green(color);
-        int blue = Color.blue(color);
-        return Color.argb(alpha, red, green, blue);
-    }
-
-    public static double[] rgbToHsv(int r, int g, int b) {
-        double rf = r / 255.0;
-        double gf = g / 255.0;
-        double bf = b / 255.0;
-        double max = (rf > gf && rf > bf) ? rf : (gf > bf) ? gf : bf;
-        double min = (rf < gf && rf < bf) ? rf : (gf < bf) ? gf : bf;
-        double h, s;
-        double d = max - min;
-
-        s = max == 0 ? 0 : d / max;
-
-        if (max == min) {
-            h = 0;
-        } else {
-            if (rf > gf && rf > bf) {
-                h = (gf - bf) / d + (gf < bf ? 6 : 0);
-            } else if (gf > bf) {
-                h = (bf - rf) / d + 2;
-            } else {
-                h = (rf - gf) / d + 4;
-            }
-
-            h /= 6;
-        }
-
-        return new double[]{ h, s, max };
-    }
-
-    public static int[] hsvToRgb(double h, double s, double v) {
-        double r = 0, g = 0, b = 0;
-        double i = (int) Math.floor(h * 6);
-        double f = h * 6 - i;
-        double p = v * (1 - s);
-        double q = v * (1 - f * s);
-        double t = v * (1 - (1 - f) * s);
-
-        switch ((int) i % 6) {
-            case 0:
-                r = v;
-                g = t;
-                b = p;
-                break;
-            case 1:
-                r = q;
-                g = v;
-                b = p;
-                break;
-            case 2:
-                r = p;
-                g = v;
-                b = t;
-                break;
-            case 3:
-                r = p;
-                g = q;
-                b = v;
-                break;
-            case 4:
-                r = t;
-                g = p;
-                b = v;
-                break;
-            case 5:
-                r = v;
-                g = p;
-                b = q;
-                break;
-        }
-
-        return new int[]{(int) (r * 255), (int) (g * 255), (int) (b * 255)};
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
-    public static void clearCursorDrawable(EditText editText) {
-        if (editText == null) {
-            return;
-        }
-
-        try {
-            Field mCursorDrawableRes = TextView.class.getDeclaredField("mCursorDrawableRes");
-            mCursorDrawableRes.setAccessible(true);
-            mCursorDrawableRes.setInt(editText, 0);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
