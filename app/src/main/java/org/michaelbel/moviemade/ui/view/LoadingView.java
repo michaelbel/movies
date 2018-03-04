761a2250aca5afc04343ede814032847b11c8bd0
Michael Bel <michael-bel@outlook.com>
2018-03-04 23:20:31 +0300

    Update background color

--- app/src/main/java/org/michaelbel/moviemade/ui/view/LoadingView.java
+++ app/src/main/java/org/michaelbel/moviemade/ui/view/LoadingView.java
@@ -1,12 +1,15 @@
 package org.michaelbel.moviemade.ui.view;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.support.annotation.IntDef;
+import android.support.v4.content.ContextCompat;
 import android.view.Gravity;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 
 import org.michaelbel.core.widget.LayoutHelper;
+import org.michaelbel.moviemade.app.Theme;
 
 public class LoadingView extends FrameLayout {
 
@@ -24,7 +27,10 @@ public class LoadingView extends FrameLayout {
     public LoadingView(Context context) {
         super(context);
 
+        setBackgroundColor(ContextCompat.getColor(context, Theme.backgroundColor()));
+
         ProgressBar progressBar = new ProgressBar(context);
+        progressBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(context, Theme.accentColor()), PorterDuff.Mode.MULTIPLY);
         progressBar.setLayoutParams(LayoutHelper.makeFrame(24, 24, Gravity.CENTER, 0, 12, 0, 12));
         addView(progressBar);
     }
