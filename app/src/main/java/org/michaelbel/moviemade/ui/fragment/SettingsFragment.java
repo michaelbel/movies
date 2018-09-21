0e664ef912167a43704cfc9935ecc749ec16e46a
Michael Bely <michael-bel@outlook.com>
2018-09-21 21:20:34 +0300

    Update fragment

--- app/src/main/java/org/michaelbel/moviemade/ui/fragment/SettingsFragment.java
+++ app/src/main/java/org/michaelbel/moviemade/ui/fragment/SettingsFragment.java
@@ -20,44 +20,25 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import org.michaelbel.bottomsheet.BottomSheet;
-import org.michaelbel.moviemade.R;
 import org.michaelbel.core.widget.LayoutHelper;
+import org.michaelbel.core.widget.RecyclerListView;
+import org.michaelbel.material.widget.Holder;
+import org.michaelbel.moviemade.R;
 import org.michaelbel.moviemade.app.Moviemade;
 import org.michaelbel.moviemade.app.Theme;
 import org.michaelbel.moviemade.app.eventbus.Events;
 import org.michaelbel.moviemade.ui.SettingsActivity;
-import org.michaelbel.moviemade.ui.adapter.recycler.Holder;
 import org.michaelbel.moviemade.ui.view.cell.EmptyCell;
 import org.michaelbel.moviemade.ui.view.cell.TextCell;
 import org.michaelbel.moviemade.ui.view.cell.TextDetailCell;
-import org.michaelbel.core.widget.RecyclerListView;
 import org.michaelbel.moviemade.utils.AndroidUtils;
 import org.michaelbel.moviemade.utils.ScreenUtils;
 
 public class SettingsFragment extends Fragment {
 
-    private int asc = 0;
-
     private int rowCount;
-    private int themeRow;
-    private int emptyRow1;
-    private int emptyRow2;
-    private int viewTypeRow;
-    //private int storageUsageRow;
-    //private int searchHistoryRow;
-    //private int imageQualityRow;
-    private int adultRow;
     private int inAppBrowserRow;
-    private int scrollToTopRow;
-    private int zoomReviewRow;
-    private int fullOverviewRow;
-    private int scrollbarsRow;
-    private int textSelectionRow;
-    private int additionalOptionsRow;
-    private int emptyRow3;
-
-    private String[] themes;
-    private String[] viewTypes;
+    private int adultRow;
 
     private SharedPreferences prefs;
     private SettingsActivity activity;
@@ -68,26 +49,7 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setHasOptionsMenu(true);
         activity = (SettingsActivity) getActivity();
-
-        themes = getResources().getStringArray(R.array.Themes);
-        viewTypes = getResources().getStringArray(R.array.ViewTypes);
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, final MenuInflater inflater) {
-        menu.add(null)
-            .setIcon(Theme.getIcon(R.drawable.ic_settings, ContextCompat.getColor(activity, Theme.primaryColor())))
-            .setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_IF_ROOM)
-            .setOnMenuItemClickListener(menuItem -> {
-                asc++;
-                if (asc == 5) {
-                    //activity.startFragment(new SettingsAdvancedFragment(), activity.binding.fragmentView, "settingsAdvancedFragment");
-                    asc = 0;
-                }
-                return true;
-            });
     }
 
     @Nullable
@@ -98,25 +60,11 @@ public class SettingsFragment extends Fragment {
         activity.toolbarTitle.setText(R.string.Settings);
 
         FrameLayout fragmentView = new FrameLayout(activity);
-        fragmentView.setBackgroundColor(ContextCompat.getColor(activity, Theme.backgroundColor()));
+        fragmentView.setBackgroundColor(ContextCompat.getColor(activity, R.color.background));
 
         rowCount = 0;
-        themeRow = rowCount++;
-        emptyRow1 = rowCount++;
         inAppBrowserRow = rowCount++;
-        scrollbarsRow = rowCount++;
-        textSelectionRow = rowCount++;
-        emptyRow2 = rowCount++;
-        viewTypeRow = rowCount++;
-        //storageUsageRow = rowCount++;
-        //searchHistoryRow = rowCount++;
-        //imageQualityRow = rowCount++;
         adultRow = rowCount++;
-        scrollToTopRow = rowCount++;
-        zoomReviewRow = rowCount++;
-        fullOverviewRow = rowCount++;
-        additionalOptionsRow = rowCount++;
-        emptyRow3 = rowCount++;
 
         prefs = activity.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
         linearLayoutManager = new LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false);
@@ -126,169 +74,19 @@ public class SettingsFragment extends Fragment {
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setLayoutParams(LayoutHelper.makeFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
         recyclerView.setOnItemClickListener((view, position) -> {
-            /*if (position == storageUsageRow) {
-                activity.startFragment(new StorageFragment(), activity.binding.fragmentView, "storageFragment");
-            } else if (position == searchHistoryRow) {
-                activity.startFragment(new SearchHistoryFragment(), activity.binding.fragmentView, "searchHistoryFragment");
-            } else if (position == imageQualityRow) {
-                BottomSheet.Builder builder = new BottomSheet.Builder(activity);
-                builder.setBackgroundColor(ContextCompat.getColor(activity, Theme.foregroundColor()));
-                builder.setItemTextColor(ContextCompat.getColor(activity, Theme.primaryTextColor()));
-                builder.setCellHeight(ScreenUtils.dp(52));
-                builder.setItems(new int[] { R.string.ImageQualityLow, R.string.ImageQualityMedium, R.string.ImageQualityHigh, R.string.ImageQualityOriginal }, (dialogInterface, i) -> {
-                    String backdropSize;
-                    String logoSize;
-                    String posterSize;
-                    String profileSize;
-                    String stillSize;
-
-                    String imageSize;
-
-                    if (i == 0) {
-                        imageSize = getString(R.string.ImageQualityLow);
-
-                        backdropSize = "w300";
-                        logoSize = "w45";
-                        posterSize = "w92";
-                        profileSize = "w45";
-                        stillSize = "w92";
-                    } else if (i == 1) {
-                        imageSize = getString(R.string.ImageQualityMedium);
-
-                        backdropSize = "w780";
-                        logoSize = "w185";
-                        posterSize = "w342";
-                        profileSize = "w185";
-                        stillSize = "w185";
-                    } else if (i == 2) {
-                        imageSize = getString(R.string.ImageQualityHigh);
-
-                        backdropSize = "w1280";
-                        logoSize = "w500";
-                        posterSize = "w780"; *//* w632 ? *//*
-                        profileSize = "w185";
-                        stillSize = "w300";
-                    } else {
-                        imageSize = getString(R.string.ImageQualityOriginal);
-
-                        backdropSize = "original";
-                        logoSize = "original";
-                        posterSize = "original";
-                        profileSize = "original";
-                        stillSize = "original";
-                    }
-
-                    SharedPreferences prefs = activity.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = prefs.edit();
-                    editor.putString("image_quality", imageSize);
-                    editor.putString("image_quality_backdrop", backdropSize);
-                    editor.putString("image_quality_logo", logoSize);
-                    editor.putString("image_quality_poster", posterSize);
-                    editor.putString("image_quality_profile", profileSize);
-                    editor.putString("image_quality_still", stillSize);
-                    editor.putBoolean("image_quality_custom", false);
-                    editor.apply();
-
-                    if (view instanceof TextDetailCell) {
-                        ((TextDetailCell) view).setValue(imageSize);
-                    }
-
-                    ((Moviemade) activity.getApplication()).eventBus().send(new Events.MovieListUpdateImageQuality());
-                });
-                builder.show();
-            } else*/
-            if (position == themeRow) {
-                activity.startFragment(new ThemeFragment(), R.id.fragment_view, "themeFragment");
-            } else if (position == viewTypeRow) {
-                BottomSheet.Builder builder = new BottomSheet.Builder(activity);
-                builder.setBackgroundColor(ContextCompat.getColor(activity, Theme.foregroundColor()));
-                builder.setItemTextColor(ContextCompat.getColor(activity, Theme.primaryTextColor()));
-                builder.setCellHeight(ScreenUtils.dp(52));
-                builder.setItems(viewTypes, (dialog, i) -> {
-                    SharedPreferences prefs = activity.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = prefs.edit();
-                    editor.putInt("view_type", i);
-                    editor.apply();
-
-                    if (view instanceof TextDetailCell) {
-                        ((TextDetailCell) view).setValue(viewTypes[i]);
-                    }
-
-                    ((Moviemade) activity.getApplication()).eventBus().send(new Events.MovieListRefreshLayout());
-                });
-                builder.show();
-            } else if (position == inAppBrowserRow) {
-                SharedPreferences prefs = activity.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
+            if (position == inAppBrowserRow) {
                 boolean enable = prefs.getBoolean("in_app_browser", true);
-                editor.putBoolean("in_app_browser", !enable);
-                editor.apply();
-                if (view instanceof TextCell) {
-                    ((TextCell) view).setChecked(!enable);
+                prefs.edit().putBoolean("in_app_browser", !enable).apply();
+                if (view instanceof TextDetailCell) {
+                    ((TextDetailCell) view).setChecked(!enable);
                 }
             } else if (position == adultRow) {
-                SharedPreferences.Editor editor = prefs.edit();
                 boolean enable = prefs.getBoolean("adult", true);
-                editor.putBoolean("adult", !enable);
-                editor.apply();
+                prefs.edit().putBoolean("adult", !enable).apply();
                 if (view instanceof TextDetailCell) {
                     ((TextDetailCell) view).setChecked(!enable);
                 }
                 ((Moviemade) activity.getApplication()).eventBus().send(new Events.MovieListUpdateAdult());
-            } else if (position == scrollToTopRow) {
-                SharedPreferences prefs = activity.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                boolean enable = prefs.getBoolean("scroll_to_top", true);
-                editor.putBoolean("scroll_to_top", !enable);
-                editor.apply();
-                if (view instanceof TextDetailCell) {
-                    ((TextDetailCell) view).setChecked(!enable);
-                }
-            } else if (position == zoomReviewRow) {
-                SharedPreferences prefs = activity.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                boolean enable = prefs.getBoolean("zoom_review", true);
-                editor.putBoolean("zoom_review", !enable);
-                editor.apply();
-                if (view instanceof TextDetailCell) {
-                    ((TextDetailCell) view).setChecked(!enable);
-                }
-            } else if (position == scrollbarsRow) {
-                SharedPreferences prefs = activity.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                boolean enable = prefs.getBoolean("scrollbars", true);
-                editor.putBoolean("scrollbars", !enable);
-                editor.apply();
-                if (view instanceof TextCell) {
-                    ((TextCell) view).setChecked(!enable);
-                }
-            } else if (position == fullOverviewRow) {
-                SharedPreferences prefs = activity.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                boolean enable = prefs.getBoolean("full_overview", false);
-                editor.putBoolean("full_overview", !enable);
-                editor.apply();
-                if (view instanceof TextDetailCell) {
-                    ((TextDetailCell) view).setChecked(!enable);
-                }
-            } else if (position == textSelectionRow) {
-                SharedPreferences prefs = activity.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                boolean enable = prefs.getBoolean("text_select", false);
-                editor.putBoolean("text_select", !enable);
-                editor.apply();
-                if (view instanceof TextCell) {
-                    ((TextCell) view).setChecked(!enable);
-                }
-            } else if (position == additionalOptionsRow) {
-                SharedPreferences prefs = activity.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                boolean enable = prefs.getBoolean("additional_options", false);
-                editor.putBoolean("additional_options", !enable);
-                editor.apply();
-                if (view instanceof TextDetailCell) {
-                    ((TextDetailCell) view).setChecked(!enable);
-                }
             }
         });
         fragmentView.addView(recyclerView);
@@ -332,80 +130,21 @@ public class SettingsFragment extends Fragment {
             } else if (type == 2) {
                 TextCell cell = (TextCell) holder.itemView;
                 cell.changeLayoutParams().setHeight(ScreenUtils.dp(52));
-
-                if (position == themeRow) {
-                    cell.setMode(TextCell.MODE_VALUE_TEXT);
-                    cell.setText(R.string.Theme);
-                    cell.setValue(themes[Theme.getTheme()]);
-                    cell.setDivider(false);
-                } else if (position == inAppBrowserRow) {
-                    cell.setMode(TextCell.MODE_SWITCH);
-                    cell.setText(R.string.InAppBrowser);
-                    cell.setChecked(prefs.getBoolean("in_app_browser", true));
-                    cell.setDivider(true);
-                } else if (position == scrollbarsRow) {
-                    cell.setMode(TextCell.MODE_SWITCH);
-                    cell.setText(R.string.Scrollbars);
-                    cell.setChecked(AndroidUtils.scrollbars());
-                    cell.setDivider(true);
-                } else if (position == textSelectionRow) {
-                    cell.setMode(TextCell.MODE_SWITCH);
-                    cell.setText(R.string.TextSelectable);
-                    cell.setChecked(AndroidUtils.textSelect());
-                    cell.setDivider(false);
-                }
-
-                /*if (position == storageUsageRow) {
-                    cell.setText(R.string.StorageUsage);
-                    cell.setDivider(true);
-                } else if (position == searchHistoryRow) {
-                    cell.setText(R.string.SearchHistory);
-                }*/
             } else if (type == 3) {
                 TextDetailCell cell = (TextDetailCell) holder.itemView;
                 cell.changeLayoutParams();
 
-                if (position == adultRow) {
+                if (position == inAppBrowserRow) {
+                    cell.setMode(TextDetailCell.MODE_SWITCH);
+                    cell.setText(R.string.InAppBrowser);
+                    cell.setValue(R.string.InAppBrowserInfo);
+                    cell.setChecked(prefs.getBoolean("in_app_browser", true));
+                    cell.setDivider(true);
+                } else if (position == adultRow) {
                     cell.setMode(TextDetailCell.MODE_SWITCH);
                     cell.setText(getString(R.string.IncludeAdult));
                     cell.setValue(R.string.IncludeAdultInfo);
                     cell.setChecked(AndroidUtils.includeAdult());
-                    cell.setDivider(true);
-                } /*else if (position == imageQualityRow) {
-                    String imageQuality = prefs.getString("image_quality", getString(R.string.ImageQualityMedium));
-                    boolean customSettings = prefs.getBoolean("image_quality_settings_customize", false);
-
-                    cell.setText(getString(R.string.ImageQuality));
-                    cell.setValue(customSettings ? "Custom" : imageQuality);
-                    cell.setDivider(true);
-                }*/ else if (position == viewTypeRow) {
-                    cell.setMode(TextDetailCell.MODE_DEFAULT);
-                    cell.setText(R.string.ViewType);
-                    cell.setValue(viewTypes[AndroidUtils.viewType()]);
-                    cell.setDivider(true);
-                } else if (position == scrollToTopRow) {
-                    cell.setMode(TextDetailCell.MODE_SWITCH);
-                    cell.setText(R.string.ScrollToTop);
-                    cell.setValue(R.string.ScrollToTopInfo);
-                    cell.setChecked(AndroidUtils.scrollToTop());
-                    cell.setDivider(true);
-                }  else if (position == zoomReviewRow) {
-                    cell.setMode(TextDetailCell.MODE_SWITCH);
-                    cell.setText(R.string.ZoomReview);
-                    cell.setValue(R.string.ZoomReviewInfo);
-                    cell.setChecked(AndroidUtils.zoomReview());
-                    cell.setDivider(true);
-                } else if (position == fullOverviewRow) {
-                    cell.setMode(TextDetailCell.MODE_SWITCH);
-                    cell.setText(R.string.FullOverview);
-                    cell.setValue(R.string.FullOverviewInfo);
-                    cell.setChecked(AndroidUtils.fullOverview());
-                    cell.setDivider(true);
-                } else if (position == additionalOptionsRow) {
-                    cell.setMode(TextDetailCell.MODE_SWITCH);
-                    cell.setText("Movie Additional Options");
-                    cell.setValue("Tap and hold on movie to show options");
-                    cell.setChecked(AndroidUtils.additionalOptions());
                     cell.setDivider(false);
                 }
             }
@@ -418,11 +157,11 @@ public class SettingsFragment extends Fragment {
 
         @Override
         public int getItemViewType(int position) {
-            if (position == emptyRow1 || position == emptyRow2 || position == emptyRow3) {
+            if (position == -9) {
                 return 1;
-            } else if (/*position == storageUsageRow || position == searchHistoryRow*/ position == themeRow || position == inAppBrowserRow || position == scrollbarsRow || position == textSelectionRow) {
+            } else if (position == -8) {
                 return 2;
-            } else if (position == viewTypeRow || /*position == imageQualityRow || */position == adultRow || position == scrollToTopRow || position == zoomReviewRow || position == fullOverviewRow || position == additionalOptionsRow) {
+            } else if (position == adultRow || position == inAppBrowserRow) {
                 return 3;
             } else {
                 return -1;
