bd7529c285a2e9b90887b5460c25784e197661b0
Michael Bely <michael-bel@outlook.com>
2018-10-31 21:01:37 +0300

    Update to 0.3.6

---
+++ app/src/main/java/org/michaelbel/moviemade/annotation/OptimizedForTablets.java
@@ -0,0 +1,19 @@
+package org.michaelbel.moviemade.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({
+    ElementType.ANNOTATION_TYPE,
+    ElementType.CONSTRUCTOR,
+    ElementType.FIELD,
+    ElementType.METHOD,
+    ElementType.TYPE
+})
+@Documented
+@SuppressWarnings("all")
+public @interface OptimizedForTablets {}
