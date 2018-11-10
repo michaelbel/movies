1625abb2a4f6e28440b2bedf5b1f61f7ba808847
Michael Bely <michael-bel@outlook.com>
2018-11-10 21:32:53 +0300

    Updated with ButterKnife

--- app/src/main/java/org/michaelbel/moviemade/ui/fragment/SettingsFragment.java
+++ app/src/main/java/org/michaelbel/moviemade/ui/fragment/SettingsFragment.java
@@ -1,6 +1,5 @@
 package org.michaelbel.moviemade.ui.fragment;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -10,29 +9,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.michaelbel.material.widget.RecyclerListView;
 import org.michaelbel.moviemade.Moviemade;
 import org.michaelbel.moviemade.R;
-import org.michaelbel.moviemade.annotation.OptimizedForTablets;
 import org.michaelbel.moviemade.eventbus.Events;
+import org.michaelbel.moviemade.extensions.DeviceUtil;
 import org.michaelbel.moviemade.ui.activity.AboutActivity;
 import org.michaelbel.moviemade.ui.activity.SettingsActivity;
-import org.michaelbel.moviemade.ui_old.view.cell.EmptyCell;
+import org.michaelbel.moviemade.ui.widget.RecyclerListView;
 import org.michaelbel.moviemade.ui_old.view.cell.TextCell;
 import org.michaelbel.moviemade.ui_old.view.cell.TextDetailCell;
-import org.michaelbel.moviemade.utils.AndroidUtils;
-import org.michaelbel.moviemade.utils.ScreenUtils;
+
+import javax.inject.Inject;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.AppCompatTextView;
-import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-@OptimizedForTablets
+@SuppressWarnings("all")
 public class SettingsFragment extends Fragment {
+
+    public static final String KEY_IN_APP_BROWSER = "in_app_browser";
+    public static final String KEY_ADULT = "adult";
 
     private int rowCount;
     private int inAppBrowserRow;
@@ -40,28 +41,31 @@ public class SettingsFragment extends Fragment {
     private int aboutRow;
 
     private SettingsAdapter adapter;
-    private SharedPreferences prefs;
     private SettingsActivity activity;
     private LinearLayoutManager linearLayoutManager;
 
-    private RecyclerListView recyclerView;
+    @Inject
+    SharedPreferences sharedPreferences;
+
+    @BindView(R.id.recycler_view)
+    public RecyclerListView recyclerView;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activity = (SettingsActivity) getActivity();
+        Moviemade.getComponent().injest(this);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        ButterKnife.bind(this, view);
 
         activity.toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
         activity.toolbar.setNavigationOnClickListener(v -> activity.finish());
-        activity.toolbarTitle.setText(R.string.Settings);
-
-        recyclerView = view.findViewById(R.id.recycler_view);
+        activity.toolbarTitle.setText(R.string.settings);
         return view;
     }
 
@@ -75,21 +79,20 @@ public class SettingsFragment extends Fragment {
         aboutRow = rowCount++;
 
         adapter = new SettingsAdapter();
-        prefs = activity.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
         linearLayoutManager = new LinearLayoutManager(activity, RecyclerView.VERTICAL, false);
 
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setOnItemClickListener((v, position) -> {
             if (position == inAppBrowserRow) {
-                boolean enable = prefs.getBoolean("in_app_browser", true);
-                prefs.edit().putBoolean("in_app_browser", !enable).apply();
+                boolean enable = sharedPreferences.getBoolean(KEY_IN_APP_BROWSER, true);
+                sharedPreferences.edit().putBoolean(KEY_IN_APP_BROWSER, !enable).apply();
                 if (v instanceof TextDetailCell) {
                     ((TextDetailCell) v).setChecked(!enable);
                 }
             } else if (position == adultRow) {
-                boolean enable = prefs.getBoolean("adult", true);
-                prefs.edit().putBoolean("adult", !enable).apply();
+                boolean enable = sharedPreferences.getBoolean(KEY_ADULT, true);
+                sharedPreferences.edit().putBoolean(KEY_ADULT, !enable).apply();
                 if (v instanceof TextDetailCell) {
                     ((TextDetailCell) v).setChecked(!enable);
                 }
@@ -104,7 +107,7 @@ public class SettingsFragment extends Fragment {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         Parcelable state = linearLayoutManager.onSaveInstanceState();
-        linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager = new LinearLayoutManager(activity, RecyclerView.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
         linearLayoutManager.onRestoreInstanceState(state);
     }
@@ -122,7 +125,7 @@ public class SettingsFragment extends Fragment {
                 view = new TextDetailCell(activity);
             }
 
-            return new org.michaelbel.moviemade.ui.widget.RecyclerListView.ViewHolder(view);
+            return new RecyclerListView.ViewHolder(view);
         }
 
         @Override
@@ -131,7 +134,7 @@ public class SettingsFragment extends Fragment {
 
             if (type == 1) {
                 TextCell cell = (TextCell) holder.itemView;
-                cell.setHeight(ScreenUtils.dp(52));
+                cell.setHeight(DeviceUtil.dp(holder.itemView.getContext(), 52));
 
                 if (position == aboutRow) {
                     cell.setMode(TextCell.MODE_DEFAULT);
@@ -144,13 +147,13 @@ public class SettingsFragment extends Fragment {
                     cell.setMode(TextDetailCell.MODE_SWITCH);
                     cell.setText(R.string.in_app_browser);
                     cell.setValue(R.string.in_app_browser_info);
-                    cell.setChecked(prefs.getBoolean("in_app_browser", true));
+                    cell.setChecked(sharedPreferences.getBoolean(KEY_IN_APP_BROWSER, true));
                     cell.setDivider(true);
                 } else if (position == adultRow) {
                     cell.setMode(TextDetailCell.MODE_SWITCH);
                     cell.setText(getString(R.string.include_adult));
                     cell.setValue(R.string.include_adult_info);
-                    cell.setChecked(prefs.getBoolean("adult", true));
+                    cell.setChecked(sharedPreferences.getBoolean(KEY_ADULT, true));
                     cell.setDivider(true);
                 }
             }
@@ -170,68 +173,4 @@ public class SettingsFragment extends Fragment {
             }
         }
     }
-
-    /*private class SettingsAdapter extends RecyclerView.Adapter {
-
-        private AppCompatTextView textView;
-
-        private AppCompatTextView cellText1;
-        private AppCompatTextView cellText2;
-        private SwitchCompat switchCompat;
-        private View dividerView;
-
-        @NonNull
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int type) {
-            View view;
-
-            if (type == 1) {
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_cell_text, parent, false);
-                textView = view.findViewById(R.id.text_view);
-            } else {
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_cell_details_switch, parent, false);
-                cellText1 = view.findViewById(R.id.text_view);
-                cellText2 = view.findViewById(R.id.value_text);
-                switchCompat = view.findViewById(R.id.switch_compat);
-                dividerView = view.findViewById(R.id.divider_view);
-            }
-
-            return new org.michaelbel.moviemade.ui.widget.RecyclerListView.ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-            int type = getItemViewType(position);
-
-            if (type == 1) {
-                textView.setText(R.string.about);
-            } else {
-                if (position == inAppBrowserRow) {
-                    cellText1.setText(R.string.in_app_browser);
-                    cellText2.setText(R.string.in_app_browser_info);
-                    switchCompat.setChecked(prefs.getBoolean("in_app_browser", true));
-                    dividerView.setVisibility(View.VISIBLE);
-                } else if (position == adultRow) {
-                    cellText1.setText(getString(R.string.include_adult));
-                    cellText2.setText(R.string.include_adult_info);
-                    switchCompat.setChecked(prefs.getBoolean("adult", true));
-                    dividerView.setVisibility(View.VISIBLE);
-                }
-            }
-        }
-
-        @Override
-        public int getItemCount() {
-            return rowCount;
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            if (position == aboutRow) {
-                return 1;
-            } else {
-                return 2;
-            }
-        }
-    }*/
 }
