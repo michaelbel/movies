f2635c30274587fbbee9d679f0cac66700ce6eb9
Michael Bely <michael-bel@outlook.com>
2018-11-10 20:54:50 +0300

    Injested new fragments

--- app/src/main/java/org/michaelbel/moviemade/di/component/DiComponent.java
+++ app/src/main/java/org/michaelbel/moviemade/di/component/DiComponent.java
@@ -4,7 +4,10 @@ import org.michaelbel.moviemade.di.module.RestModule;
 import org.michaelbel.moviemade.di.module.SharedPrefsModule;
 import org.michaelbel.moviemade.mvp.presenter.MoviePresenter;
 import org.michaelbel.moviemade.ui.activity.MainActivity;
+import org.michaelbel.moviemade.ui.fragment.NowPlayingFragment;
 import org.michaelbel.moviemade.ui.fragment.SettingsFragment;
+import org.michaelbel.moviemade.ui.fragment.TopRatedFragment;
+import org.michaelbel.moviemade.ui.fragment.UpcomingFragment;
 
 import javax.inject.Singleton;
 
@@ -19,6 +22,9 @@ public interface DiComponent {
 
     void injest(MainActivity activity);
     void injest(SettingsFragment fragment);
+    void injest(NowPlayingFragment fragment);
+    void injest(TopRatedFragment fragment);
+    void injest(UpcomingFragment fragment);
 
     void injest(MoviePresenter fragment);
 }
