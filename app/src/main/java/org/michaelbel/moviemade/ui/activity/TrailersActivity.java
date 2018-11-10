a3f58ac1ebaadc3fd405cee26451ca7fcd5e468e
Michael Bely <michael-bel@outlook.com>
2018-11-10 20:32:23 +0300

    Added ButterKnife library

--- app/src/main/java/org/michaelbel/moviemade/ui/activity/TrailersActivity.java
+++ app/src/main/java/org/michaelbel/moviemade/ui/activity/TrailersActivity.java
@@ -8,35 +8,38 @@ import org.michaelbel.moviemade.ui.fragment.TrailersFragment;
 
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.appcompat.widget.Toolbar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class TrailersActivity extends BaseActivity {
 
     public int movieId;
-
     private TrailersFragment fragment;
 
+    @BindView(R.id.toolbar)
     public Toolbar toolbar;
+
+    @BindView(R.id.toolbar_title)
     public AppCompatTextView toolbarTitle;
+
+    @BindView(R.id.toolbar_subtitle)
     public AppCompatTextView toolbarSubtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trailers);
+        ButterKnife.bind(this);
 
         movieId = getIntent().getIntExtra("id", 0);
         String movieTitle = getIntent().getStringExtra("title");
 
-        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
         toolbar.setNavigationOnClickListener(v -> finish());
         toolbar.setOnClickListener(v -> fragment.recyclerView.smoothScrollToPosition(0));
 
-        toolbarTitle = findViewById(R.id.toolbar_title);
         toolbarTitle.setText(R.string.trailers);
-
-        toolbarSubtitle = findViewById(R.id.toolbar_subtitle);
         toolbarSubtitle.setText(movieTitle);
 
         fragment = (TrailersFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
