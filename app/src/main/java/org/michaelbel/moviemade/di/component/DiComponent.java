bf4a66e4f2a21407915bb28804999b545e5b2a35
Michael Bely <michael-bel@outlook.com>
2018-11-16 01:24:15 +0300

    Added new injests

--- app/src/main/java/org/michaelbel/moviemade/di/component/DiComponent.java
+++ app/src/main/java/org/michaelbel/moviemade/di/component/DiComponent.java
@@ -7,6 +7,8 @@ import org.michaelbel.moviemade.ui.modules.movie.MoviePresenter;
 import org.michaelbel.moviemade.ui.modules.main.MainActivity;
 import org.michaelbel.moviemade.ui.modules.movie.MovieFragment;
 import org.michaelbel.moviemade.ui.modules.main.fragments.NowPlayingFragment;
+import org.michaelbel.moviemade.ui.modules.reviews.activity.ReviewActivity;
+import org.michaelbel.moviemade.ui.modules.reviews.fragment.ReviewFragment;
 import org.michaelbel.moviemade.ui.modules.settings.SettingsFragment;
 import org.michaelbel.moviemade.ui.modules.main.fragments.TopRatedFragment;
 import org.michaelbel.moviemade.ui.modules.main.fragments.UpcomingFragment;
@@ -23,11 +25,12 @@ import dagger.Component;
 })
 public interface DiComponent {
     void injest(MainActivity activity);
+    void injest(ReviewActivity activity);
     void injest(SettingsFragment fragment);
     void injest(NowPlayingFragment fragment);
     void injest(TopRatedFragment fragment);
     void injest(UpcomingFragment fragment);
     void injest(MovieFragment fragment);
-
     void injest(MoviePresenter fragment);
+    void injest(ReviewFragment fragment);
 }
