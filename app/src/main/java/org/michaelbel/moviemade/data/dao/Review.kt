22f222fd805989af51ed74c4c72ce29550eb26ff
Michael Bely <michael-bel@outlook.com>
2018-11-16 01:23:26 +0300

    Fixed errors

--- app/src/main/java/org/michaelbel/moviemade/data/dao/Review.kt
+++ app/src/main/java/org/michaelbel/moviemade/data/dao/Review.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class Review(
-    @SerializedName("id") val id: Int,
+    @SerializedName("id") val id: String,
     @SerializedName("author") val author: String,
     @SerializedName("content") val content: String,
     @SerializedName("iso_639_1") val lang: String,
--- app/src/main/java/org/michaelbel/moviemade/data/dao/Video.kt
+++ app/src/main/java/org/michaelbel/moviemade/data/dao/Video.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class Video(
-    @SerializedName("id") val id: Int,
+    @SerializedName("id") val id: String,
     @SerializedName("iso_639_1") val lang: String,
     @SerializedName("iso_3166_1") val country: String,
     @SerializedName("key") val key: String,
