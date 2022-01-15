8981da7c28abd714a0fa335297b5e1cd0be4bde0
Michael Bely <michael-bel@outlook.com>
2022-01-15 16:52:50 +0300

    Update project

--- app/src/main/java/org/michaelbel/moviemade/presentation/features/main/adapter/MoviesPagingAdapter.kt
+++ app/src/main/java/org/michaelbel/moviemade/presentation/features/main/adapter/MoviesPagingAdapter.kt
@@ -4,41 +4,40 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import org.michaelbel.data.remote.model.MovieResponse
-import org.michaelbel.moviemade.R
 import org.michaelbel.moviemade.databinding.ListitemMovieLinearBinding
 
 class MoviesPagingAdapter(
     private val clickAction: (movie: MovieResponse) -> Unit
-): PagingDataAdapter<MovieResponse, ViewHolder>(UIMODEL_COMPARATOR) {
+): PagingDataAdapter<MovieResponse, MovieViewHolder>(DIFF_UTIL_COMPARATOR) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         return MovieViewHolder(
             ListitemMovieLinearBinding.inflate(LayoutInflater.from(parent.context), parent, false),
             clickAction
         )
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return R.layout.listitem_movie_linear
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movieResponse: MovieResponse? = getItem(position)
+        movieResponse?.let { holder.bind(it) }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val uiModel = getItem(position)
-        uiModel?.let {
-            (holder as MovieViewHolder).bind(uiModel)
-        }
-    }
-
-    companion object {
-        private val UIMODEL_COMPARATOR = object: DiffUtil.ItemCallback<MovieResponse>() {
-            override fun areItemsTheSame(oldItem: MovieResponse, newItem: MovieResponse): Boolean {
+    private companion object {
+        private val DIFF_UTIL_COMPARATOR = object: DiffUtil.ItemCallback<MovieResponse>() {
+            override fun areItemsTheSame(
+                oldItem: MovieResponse,
+                newItem: MovieResponse
+            ): Boolean {
                 return oldItem.title == newItem.title
             }
 
-            override fun areContentsTheSame(oldItem: MovieResponse, newItem: MovieResponse): Boolean =
-                    oldItem == newItem
+            override fun areContentsTheSame(
+                oldItem: MovieResponse,
+                newItem: MovieResponse
+            ): Boolean {
+                return oldItem == newItem
+            }
         }
     }
 }
