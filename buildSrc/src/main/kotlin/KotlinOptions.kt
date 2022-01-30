d42f382af17ead34aff930fce645cba380ed2686
Michael Bely <michael-bel@outlook.com>
2022-01-30 16:44:09 +0300

    Update Project

--- buildSrc/src/main/kotlin/KotlinOptions.kt
+++ buildSrc/src/main/kotlin/KotlinOptions.kt
@@ -4,4 +4,5 @@ object KotlinOptions {
     const val OptExperimentalMaterial3Api = "-Xopt-in=androidx.compose.material3.ExperimentalMaterial3Api"
     const val OptExperimentalFoundationApi = "-Xopt-in=androidx.compose.foundation.ExperimentalFoundationApi"
     const val OptExperimentalSerializationApi = "-Xopt-in=kotlinx.serialization.ExperimentalSerializationApi"
+    const val OptExperimentalCoroutinesApi = "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
 }
