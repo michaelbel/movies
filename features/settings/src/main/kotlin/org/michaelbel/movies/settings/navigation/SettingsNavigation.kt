ae8fa0d98e8501ff79b50518a7499b702862fb33
michaelbel <michael-bel@outlook.com>
2022-10-08 18:05:48 +0300

    Update project

--- features/settings/src/main/kotlin/org/michaelbel/movies/settings/navigation/SettingsNavigation.kt
+++ features/settings/src/main/kotlin/org/michaelbel/movies/settings/navigation/SettingsNavigation.kt
@@ -4,13 +4,12 @@ import androidx.navigation.NavDeepLink
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navDeepLink
-import org.michaelbel.movies.core.shortcuts.MoviesShortcuts
+import org.michaelbel.movies.core.shortcuts.INTENT_ACTION_SETTINGS
 import org.michaelbel.movies.navigation.MoviesNavigationDestination
 import org.michaelbel.movies.settings.ui.SettingsRoute
-import org.michaelbel.movies.settings.ui.SettingsScreenContent
 
 private val SETTINGS_SHORTCUT_NAV_DEEP_LINK: NavDeepLink = navDeepLink {
-    action = MoviesShortcuts.INTENT_ACTION_SETTINGS
+    uriPattern = INTENT_ACTION_SETTINGS
 }
 
 object SettingsDestination: MoviesNavigationDestination {
