98bfb678db0ad23f8119efc7100b51d1fb06177b
Michael Bely <michael-bel@outlook.com>
2022-10-09 18:15:27 +0300

    Develop (#85)
    * Update project
    
    * Update project
    
    * Update project
    
    * Update project

---
+++ core/src/main/kotlin/org/michaelbel/movies/core/config/RemoteParams.kt
@@ -0,0 +1,5 @@
+package org.michaelbel.movies.core.config
+
+object RemoteParams {
+    const val PARAM_SETTINGS_ICON_VISIBLE = "param_settings_icon_visible"
+}
---
+++ core/src/main/kotlin/org/michaelbel/movies/core/config/di/RemoteConfigModule.kt
@@ -0,0 +1,36 @@
+package org.michaelbel.movies.core.config.di
+
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
+import org.michaelbel.movies.core.config.RemoteParams
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteConfigModule {
+
+    private const val FETCH_INTERVAL_IN_SECONDS = 5L
+
+    @Provides
+    fun provideFirebaseRemoteConfig(): FirebaseRemoteConfig {
+        val configSettings: FirebaseRemoteConfigSettings = remoteConfigSettings {
+            fetchTimeoutInSeconds = FETCH_INTERVAL_IN_SECONDS
+            minimumFetchIntervalInSeconds = FETCH_INTERVAL_IN_SECONDS
+        }
+        val defaults: Map<String, Any> = mapOf(
+            RemoteParams.PARAM_SETTINGS_ICON_VISIBLE to true
+        )
+        val firebaseRemoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig.apply {
+            setConfigSettingsAsync(configSettings)
+            setDefaultsAsync(defaults)
+        }
+        return firebaseRemoteConfig
+    }
+}
