c3d9ce94627d92393102d01ae2c81ac1ad87b0ea
michaelbel <michael-bel@outlook.com>
2022-10-13 13:16:31 +0300

    Update project

--- feature/details/src/main/kotlin/org/michaelbel/movies/details/ui/DetailsFailure.kt
+++ feature/details/src/main/kotlin/org/michaelbel/movies/details/ui/DetailsFailure.kt
@@ -1,6 +1,5 @@
 package org.michaelbel.movies.details.ui
 
-import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Icon
@@ -11,14 +10,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import org.michaelbel.movies.details.R
-import org.michaelbel.movies.ui.theme.MoviesTheme
 import org.michaelbel.movies.ui.icon.MoviesIcons
+import org.michaelbel.movies.ui.preview.DevicePreviews
+import org.michaelbel.movies.ui.theme.MoviesTheme
 
 @Composable
 internal fun DetailsFailure(
@@ -62,9 +61,8 @@ internal fun DetailsFailure(
     }
 }
 
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
+@DevicePreviews
 private fun DetailsFailurePreview() {
     MoviesTheme {
         DetailsFailure(
