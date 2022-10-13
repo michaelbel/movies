07b09c8c9b64ea70103c191c27e29782a300ca35
michaelbel <michael-bel@outlook.com>
2022-10-13 13:32:44 +0300

    Update project

--- core/ui/src/main/kotlin/org/michaelbel/movies/ui/preview/DevicePreview.kt
+++
@@ -1,8 +0,0 @@
-package org.michaelbel.movies.ui.preview
-
-import android.content.res.Configuration
-import androidx.compose.ui.tooling.preview.Preview
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
-annotation class DevicePreviews
