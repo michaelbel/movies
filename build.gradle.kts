207869a79ebc97ea089dd60556a91f5e40df21ab
michaelbel <michael-bel@outlook.com>
2022-10-14 21:24:56 +0300

    Update project

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,6 @@ plugins {
 }
 
 buildscript {
-    repositories {
-        mavenCentral()
-    }
-
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
