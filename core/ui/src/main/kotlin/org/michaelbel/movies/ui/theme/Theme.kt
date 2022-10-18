aa893184b6a7b10353b2d802a254df58fd2ff707
michaelbel <michael-bel@outlook.com>
2022-10-18 18:58:54 +0300

    Update project

--- core/ui/src/main/kotlin/org/michaelbel/movies/ui/theme/Theme.kt
+++ core/ui/src/main/kotlin/org/michaelbel/movies/ui/theme/Theme.kt
@@ -4,10 +4,8 @@ import android.content.Context
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.systemuicontroller.SystemUiController
