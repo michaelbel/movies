b50c83d4252ccd505430d2ee0d7a0400b6b8457c
michaelbel <michael-bel@outlook.com>
2022-11-30 17:24:17 +0300

    Details placeholder

--- core/entities/src/main/kotlin/org/michaelbel/movies/entities/MovieDetailsData.kt
+++ core/entities/src/main/kotlin/org/michaelbel/movies/entities/MovieDetailsData.kt
@@ -1,11 +1,11 @@
 package org.michaelbel.movies.entities
 
 data class MovieDetailsData(
-    val id: Int,
-    val overview: String,
-    val posterPath: String,
-    val backdropPath: String,
-    val releaseDate: String,
-    val title: String,
-    val voteAverage: Float
+    val id: Int = 0,
+    val overview: String = "",
+    val posterPath: String = "",
+    val backdropPath: String = "",
+    val releaseDate: String = "",
+    val title: String = "",
+    val voteAverage: Float = 0F
 )
--- feature/details/src/main/kotlin/org/michaelbel/movies/details/ui/DetailsLoading.kt
+++ feature/details/src/main/kotlin/org/michaelbel/movies/details/ui/DetailsLoading.kt
@@ -2,12 +2,10 @@ package org.michaelbel.movies.details.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
+import org.michaelbel.movies.entities.MovieDetailsData
 import org.michaelbel.movies.ui.preview.DevicePreviews
 import org.michaelbel.movies.ui.theme.MoviesTheme
 
@@ -15,23 +13,11 @@ import org.michaelbel.movies.ui.theme.MoviesTheme
 internal fun DetailsLoading(
     modifier: Modifier
 ) {
-    ConstraintLayout(
-        modifier = modifier
-    ) {
-        val (progress) = createRefs()
-
-        CircularProgressIndicator(
-            modifier = Modifier
-                .constrainAs(progress) {
-                    width = Dimension.wrapContent
-                    height = Dimension.wrapContent
-                    start.linkTo(parent.start)
-                    top.linkTo(parent.top)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
-                }
-        )
-    }
+    DetailsContent(
+        modifier = modifier,
+        movie = MovieDetailsData(),
+        placeHolder = true
+    )
 }
 
 @Composable
