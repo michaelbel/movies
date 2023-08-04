cb1ca6d1e5e0881ba2c186489173a8f8f28e9a25
michaelbel <michael-bel@outlook.com>
2023-08-04 13:08:55 +0300

    Update project

--- core/common/src/main/kotlin/org/michaelbel/movies/common/version/AppVersionData.kt
+++ core/common/src/main/kotlin/org/michaelbel/movies/common/version/AppVersionData.kt
@@ -2,9 +2,14 @@ package org.michaelbel.movies.common.version
 
 data class AppVersionData(
     val version: String,
-    val code: Long
+    val code: Long,
+    val isDebug: Boolean
 ) {
     companion object {
-        val None: AppVersionData = AppVersionData("", 0L)
+        val None: AppVersionData = AppVersionData(
+            version = "",
+            code = 0L,
+            isDebug = false
+        )
     }
 }
