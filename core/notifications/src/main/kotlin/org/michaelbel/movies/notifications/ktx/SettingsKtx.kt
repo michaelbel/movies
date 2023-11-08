7e395500060295031d4c61e784277a0395e6645e
michaelbel <michael-bel@outlook.com>
2023-11-08 13:46:28 +0300

    Update project

--- core/notifications/src/main/kotlin/org/michaelbel/movies/notifications/ktx/SettingsKtx.kt
+++ core/notifications/src/main/kotlin/org/michaelbel/movies/notifications/ktx/SettingsKtx.kt
@@ -11,18 +11,18 @@ val Context.appNotificationSettingsIntent: Intent
         val intent = Intent()
         when {
             Build.VERSION.SDK_INT >= 26 -> {
-                intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                intent.action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
                 intent.putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
             }
             Build.VERSION.SDK_INT >= 21 -> {
-                intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS")
+                intent.action = "android.settings.APP_NOTIFICATION_SETTINGS"
                 intent.putExtra("app_package", packageName)
                 intent.putExtra("app_uid", applicationInfo.uid)
             }
             else -> {
-                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
                 intent.addCategory(Intent.CATEGORY_DEFAULT)
-                intent.setData("package:$packageName".toUri())
+                intent.data = "package:$packageName".toUri()
             }
         }
         return intent
