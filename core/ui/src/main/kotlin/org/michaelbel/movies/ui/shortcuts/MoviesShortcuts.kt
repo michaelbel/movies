fbe8006f70f633abcd449ebe565635157d0f5e8d
michaelbel <michael-bel@outlook.com>
2023-12-02 19:53:27 +0300

    Search screen

--- core/ui/src/main/kotlin/org/michaelbel/movies/ui/shortcuts/MoviesShortcuts.kt
+++ core/ui/src/main/kotlin/org/michaelbel/movies/ui/shortcuts/MoviesShortcuts.kt
@@ -7,16 +7,26 @@ import androidx.core.content.pm.ShortcutManagerCompat
 import androidx.core.graphics.drawable.IconCompat
 import androidx.core.net.toUri
 import org.michaelbel.movies.ui.R
+import org.michaelbel.movies.ui.appicon.shortcutSearchIconRes
 import org.michaelbel.movies.ui.appicon.shortcutSettingsIconRes
 
+private const val SEARCH_SHORTCUT_ID = "searchShortcutId"
 private const val SETTINGS_SHORTCUT_ID = "settingsShortcutId"
 
-const val INTENT_ACTION_SETTINGS = "shortcut://settings"
+const val INTENT_ACTION_SEARCH = "movies_shortcut://search"
+const val INTENT_ACTION_SETTINGS = "movies_shortcut://settings"
 
 /**
  * See [App Shortcuts Design Guidelines](https://commondatastorage.googleapis.com/androiddevelopers/shareables/design/app-shortcuts-design-guidelines.pdf)
  */
 fun Context.installShortcuts() {
+    val searchShortcut = ShortcutInfoCompat.Builder(this, SEARCH_SHORTCUT_ID)
+        .setShortLabel(getString(R.string.shortcuts_search_title))
+        .setLongLabel(getString(R.string.shortcuts_search_title))
+        .setRank(1)
+        .setIcon(IconCompat.createWithResource(this, shortcutSearchIconRes))
+        .setIntent(Intent(Intent.ACTION_VIEW, INTENT_ACTION_SEARCH.toUri()))
+        .build()
     val settingsShortcut = ShortcutInfoCompat.Builder(this, SETTINGS_SHORTCUT_ID)
         .setShortLabel(getString(R.string.shortcuts_settings_title))
         .setLongLabel(getString(R.string.shortcuts_settings_title))
@@ -24,5 +34,5 @@ fun Context.installShortcuts() {
         .setIcon(IconCompat.createWithResource(this, shortcutSettingsIconRes))
         .setIntent(Intent(Intent.ACTION_VIEW, INTENT_ACTION_SETTINGS.toUri()))
         .build()
-    ShortcutManagerCompat.pushDynamicShortcut(this, settingsShortcut)
+    ShortcutManagerCompat.setDynamicShortcuts(this, listOf(searchShortcut, settingsShortcut))
 }
--- feature/search/src/main/kotlin/org/michaelbel/movies/search/SearchNavigation.kt
+++ feature/search/src/main/kotlin/org/michaelbel/movies/search/SearchNavigation.kt
@@ -1,9 +1,16 @@
 package org.michaelbel.movies.search
 
 import androidx.navigation.NavController
+import androidx.navigation.NavDeepLink
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
 import org.michaelbel.movies.search.ui.SearchRoute
+import org.michaelbel.movies.ui.shortcuts.INTENT_ACTION_SEARCH
+
+private val SEARCH_SHORTCUT_NAV_DEEP_LINK: NavDeepLink = navDeepLink {
+    uriPattern = INTENT_ACTION_SEARCH
+}
 
 fun NavController.navigateToSearch() {
     navigate(SearchDestination.route)
@@ -14,7 +21,8 @@ fun NavGraphBuilder.searchGraph(
     navigateToDetails: (Int) -> Unit,
 ) {
     composable(
-        route = SearchDestination.route
+        route = SearchDestination.route,
+        deepLinks = listOf(SEARCH_SHORTCUT_NAV_DEEP_LINK)
     ) {
         SearchRoute(
             onBackClick = navigateBack,
