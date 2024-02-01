c84cb26b9a1027547c5029eb581ddbe3ba8b99de
michaelbel <michael-bel@outlook.com>
2024-02-01 15:10:29 +0300

    Update project

--- core/interactor/src/main/kotlin/org/michaelbel/movies/interactor/MovieInteractor.kt
+++ core/interactor/src/main/kotlin/org/michaelbel/movies/interactor/MovieInteractor.kt
@@ -1,19 +1,20 @@
 package org.michaelbel.movies.interactor
 
+import androidx.paging.PagingData
 import androidx.paging.PagingSource
 import kotlinx.coroutines.flow.Flow
+import org.michaelbel.movies.common.list.MovieList
 import org.michaelbel.movies.network.Either
 import org.michaelbel.movies.network.model.MovieResponse
-import org.michaelbel.movies.network.model.Result
 import org.michaelbel.movies.persistence.database.entity.MovieDb
 
 interface MovieInteractor {
 
+    fun moviesPagingData(movieList: MovieList): Flow<PagingData<MovieDb>>
+
     fun moviesPagingSource(pagingKey: String): PagingSource<Int, MovieDb>
 
     fun moviesFlow(pagingKey: String, limit: Int): Flow<List<MovieDb>>
-
-    suspend fun moviesResult(pagingKey: String, page: Int): Result<MovieResponse>
 
     suspend fun movie(pagingKey: String, movieId: Int): MovieDb
 
--- core/interactor/src/main/kotlin/org/michaelbel/movies/interactor/impl/MovieInteractorImpl.kt
+++ core/interactor/src/main/kotlin/org/michaelbel/movies/interactor/impl/MovieInteractorImpl.kt
@@ -1,23 +1,47 @@
 package org.michaelbel.movies.interactor.impl
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import androidx.paging.PagingSource
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import org.michaelbel.movies.common.dispatchers.MoviesDispatchers
+import org.michaelbel.movies.common.list.MovieList
 import org.michaelbel.movies.interactor.MovieInteractor
+import org.michaelbel.movies.interactor.ktx.nameOrLocalList
+import org.michaelbel.movies.interactor.remote.MoviesRemoteMediator
 import org.michaelbel.movies.network.Either
 import org.michaelbel.movies.network.model.MovieResponse
-import org.michaelbel.movies.network.model.Result
+import org.michaelbel.movies.persistence.database.AppDatabase
 import org.michaelbel.movies.persistence.database.entity.MovieDb
 import org.michaelbel.movies.repository.MovieRepository
+import org.michaelbel.movies.repository.PagingKeyRepository
 
 @Singleton
 internal class MovieInteractorImpl @Inject constructor(
     private val dispatchers: MoviesDispatchers,
-    private val movieRepository: MovieRepository
+    private val movieRepository: MovieRepository,
+    private val pagingKeyRepository: PagingKeyRepository,
+    private val database: AppDatabase,
 ): MovieInteractor {
+
+    override fun moviesPagingData(movieList: MovieList): Flow<PagingData<MovieDb>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = MovieResponse.DEFAULT_PAGE_SIZE
+            ),
+            remoteMediator = MoviesRemoteMediator(
+                movieRepository = movieRepository,
+                pagingKeyRepository = pagingKeyRepository,
+                database = database,
+                movieList = movieList.name
+            ),
+            pagingSourceFactory = { movieRepository.moviesPagingSource(movieList.nameOrLocalList) }
+        ).flow
+    }
 
     override fun moviesPagingSource(pagingKey: String): PagingSource<Int, MovieDb> {
         return movieRepository.moviesPagingSource(pagingKey)
@@ -28,12 +52,6 @@ internal class MovieInteractorImpl @Inject constructor(
             pagingKey = pagingKey,
             limit = limit
         )
-    }
-
-    override suspend fun moviesResult(pagingKey: String, page: Int): Result<MovieResponse> {
-        return withContext(dispatchers.io) {
-            movieRepository.moviesResult(pagingKey, page)
-        }
     }
 
     override suspend fun movie(pagingKey: String, movieId: Int): MovieDb {
