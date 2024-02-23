346e72b89711ae6b411377236153678dc443ad8f
michaelbel <michael-bel@outlook.com>
2024-02-23 19:46:51 +0300

    Glance app widget

--- core/persistence/src/main/kotlin/org/michaelbel/movies/persistence/database/entity/mini/MovieDbMini.kt
+++ core/persistence/src/main/kotlin/org/michaelbel/movies/persistence/database/entity/mini/MovieDbMini.kt
@@ -4,12 +4,4 @@ data class MovieDbMini(
     val movieList: String,
     val id: Int,
     val title: String,
-) {
-    companion object {
-        val Empty = MovieDbMini(
-            movieList = "",
-            id = 0,
-            title = ""
-        )
-    }
-}
+)
