36875ab9e8b9ff82d0d57af6c62fbf2e5ff73f0c
michaelbel <michael-bel@outlook.com>
2024-03-17 20:48:38 +0300

    Multiplatform

--- shared/src/desktopMain/kotlin/org/michaelbel/movies/shared/Platform.desktop.kt
+++
@@ -1,7 +0,0 @@
-package org.michaelbel.movies.shared
-
-class DesktopPlatform: Platform {
-    override val name: String = "Desktop"
-}
-
-actual fun getPlatform(): Platform = DesktopPlatform()
--- shared/src/iosMain/kotlin/org/michaelbel/movies/shared/Platform.ios.kt
+++
@@ -1,9 +0,0 @@
-package org.michaelbel.movies.shared
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
