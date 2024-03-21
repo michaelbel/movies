ffe0086ff020606a3088bd5c94ba7c2beec8ee75
michaelbel <michael-bel@outlook.com>
2024-03-21 20:36:30 +0300

    Multiplatform

---
+++ core/work-kmp/src/androidMain/kotlin/org/michaelbel/movies/work/AccountUpdateWorker.kt
@@ -0,0 +1,44 @@
+@file:Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+
+package org.michaelbel.movies.work
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import org.michaelbel.movies.common.ktx.isTimePasses
+import org.michaelbel.movies.interactor.Interactor
+import org.michaelbel.movies.network.config.isTmdbApiKeyEmpty
+import java.util.concurrent.TimeUnit
+
+@HiltWorker
+actual class AccountUpdateWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val interactor: Interactor
+): CoroutineWorker(context, workerParams) {
+
+    override suspend fun doWork(): Result {
+        return try {
+            val accountId = interactor.accountId()
+            if (isTmdbApiKeyEmpty || accountId == 0) {
+                return Result.success()
+            }
+
+            val expireTime = interactor.accountExpireTime() ?: 0L
+            val currentTime = System.currentTimeMillis()
+            if (isTimePasses(ONE_DAY_MILLS, expireTime, currentTime)) {
+                interactor.accountDetails()
+            }
+            Result.success()
+        } catch (ignored: Exception) {
+            Result.failure()
+        }
+    }
+
+    private companion object {
+        private val ONE_DAY_MILLS = TimeUnit.DAYS.toMillis(1)
+    }
+}
