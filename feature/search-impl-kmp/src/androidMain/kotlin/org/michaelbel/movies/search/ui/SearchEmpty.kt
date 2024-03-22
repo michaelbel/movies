04a4f79af084f493a3ced1d428be5e7728eeb34c
michaelbel <michael-bel@outlook.com>
2024-03-22 19:28:16 +0300

    Update project

--- feature/search-impl-kmp/src/androidMain/kotlin/org/michaelbel/movies/search/ui/SearchEmpty.kt
+++ feature/search-impl-kmp/src/androidMain/kotlin/org/michaelbel/movies/search/ui/SearchEmpty.kt
@@ -1,18 +1,23 @@
 package org.michaelbel.movies.search.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import org.michaelbel.movies.common.theme.AppTheme
 import org.michaelbel.movies.search_impl_kmp.R
 import org.michaelbel.movies.ui.accessibility.MoviesContentDescription
@@ -24,34 +29,24 @@ import org.michaelbel.movies.ui.theme.MoviesTheme
 internal fun SearchEmpty(
     modifier: Modifier = Modifier
 ) {
-    ConstraintLayout(
-        modifier = modifier
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val (image, text) = createRefs()
-
         Icon(
             imageVector = MoviesIcons.ManageSearch,
             contentDescription = MoviesContentDescription.None,
-            modifier = Modifier.constrainAs(image) {
-                width = Dimension.value(36.dp)
-                height = Dimension.value(36.dp)
-                start.linkTo(parent.start)
-                top.linkTo(parent.top)
-                end.linkTo(parent.end)
-                bottom.linkTo(parent.bottom, 8.dp)
-            },
+            modifier = Modifier.size(36.dp),
             tint = MaterialTheme.colorScheme.error
         )
 
         Text(
             text = stringResource(R.string.search_results_empty),
-            modifier = Modifier.constrainAs(text) {
-                width = Dimension.fillToConstraints
-                height = Dimension.wrapContent
-                start.linkTo(parent.start, 16.dp)
-                top.linkTo(image.bottom, 8.dp)
-                end.linkTo(parent.end, 16.dp)
-            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(start = 16.dp, top = 8.dp, end = 16.dp),
             textAlign = TextAlign.Center,
             style = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.onPrimaryContainer)
         )
