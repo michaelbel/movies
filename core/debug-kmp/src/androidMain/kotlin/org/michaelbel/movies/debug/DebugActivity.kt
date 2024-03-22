2c4346990c4c6808b22c97a3e52f92e42d7e5684
michaelbel <michael-bel@outlook.com>
2024-03-22 18:59:50 +0300

    Debug settings

--- core/debug-kmp/src/androidMain/kotlin/org/michaelbel/movies/debug/DebugActivity.kt
+++ core/debug-kmp/src/androidMain/kotlin/org/michaelbel/movies/debug/DebugActivity.kt
@@ -1,14 +1,14 @@
 package org.michaelbel.movies.debug
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.fragment.app.FragmentActivity
 import dagger.hilt.android.AndroidEntryPoint
 import org.michaelbel.movies.debug.ui.DebugActivityContent
 
 @AndroidEntryPoint
-internal class DebugActivity: FragmentActivity() {
+internal class DebugActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
