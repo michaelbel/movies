13994b303b6e20267e45581a0a532f0b81869dfb
michaelbel <michael-bel@outlook.com>
2024-07-20 23:48:28 +0300

    Prepare for iOS

--- iosAppCompose/build.gradle.kts
+++ iosAppCompose/build.gradle.kts
@@ -5,12 +5,20 @@ plugins {
 }
 
 kotlin {
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
+    iosX64("iosX64") {
+        binaries.framework {
+            baseName = "iosAppCompose"
+            isStatic = true
+        }
+    }
+    iosArm64("iosArm64") {
+        binaries.framework {
+            baseName = "iosAppCompose"
+            isStatic = true
+        }
+    }
+    iosSimulatorArm64("iosSimulatorArm64") {
+        binaries.framework {
             baseName = "iosAppCompose"
             isStatic = true
         }
@@ -18,13 +26,41 @@ kotlin {
     
     sourceSets {
         commonMain.dependencies {
-            //implementation(project(":feature:main-impl"))
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+        }
+        val iosX64Main by getting
+        iosX64Main.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+        }
+        val iosArm64Main by getting
+        iosArm64Main.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+        }
+        val iosSimulatorArm64Main by getting
+        iosSimulatorArm64Main.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+
+            //implementation(project(":feature:feed"))
         }
     }
 }
