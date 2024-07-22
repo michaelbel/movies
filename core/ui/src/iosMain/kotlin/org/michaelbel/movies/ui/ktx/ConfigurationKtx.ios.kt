33278c79146573a0173e6386484f1e724c92bca1
michaelbel <michael-bel@outlook.com>
2024-07-22 19:07:49 +0300

    iOS app

--- core/ui/src/iosMain/kotlin/org/michaelbel/movies/ui/ktx/ConfigurationKtx.ios.kt
+++ core/ui/src/iosMain/kotlin/org/michaelbel/movies/ui/ktx/ConfigurationKtx.ios.kt
@@ -1,9 +1,11 @@
 package org.michaelbel.movies.ui.ktx
 
 import androidx.compose.runtime.Composable
+import platform.UIKit.UIDevice
+import platform.UIKit.UIDeviceOrientation
 
 actual val isPortrait: Boolean
-    get() = true
+    get() = UIDevice.currentDevice.orientation in listOf(UIDeviceOrientation.UIDeviceOrientationPortrait, UIDeviceOrientation.UIDeviceOrientationPortraitUpsideDown)
 
 actual fun statusBarStyle(detectDarkMode: Boolean): Any {
     return Any()
