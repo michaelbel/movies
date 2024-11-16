5988255c69f83af4c36ab5ea3136c8aa1c3d551a
michaelbel <michael-bel@outlook.com>
2024-11-16 12:05:04 +0300

    Update project

--- feature/search-impl/src/androidMain/kotlin/org/michaelbel/movies/search/ui/SearchHistoryHeader.kt
+++ feature/search-impl/src/androidMain/kotlin/org/michaelbel/movies/search/ui/SearchHistoryHeader.kt
@@ -1,19 +1,21 @@
 package org.michaelbel.movies.search.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import org.michaelbel.movies.search_impl.R
 import org.michaelbel.movies.ui.theme.MoviesTheme
 
@@ -22,7 +24,29 @@ internal fun SearchHistoryHeader(
     onClearButtonClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    ConstraintLayout(
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(R.string.search_recent),
+            textAlign = TextAlign.Start,
+            style = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.onPrimaryContainer)
+        )
+
+        TextButton(
+            onClick = onClearButtonClick
+        ) {
+            Text(
+                text = stringResource(R.string.search_clear)
+            )
+        }
+    }
+
+    /*ConstraintLayout(
         modifier = modifier
     ) {
         val (recentSearchesText, clearButton) = createRefs()
@@ -54,7 +78,7 @@ internal fun SearchHistoryHeader(
                 text = stringResource(R.string.search_clear)
             )
         }
-    }
+    }*/
 }
 
 @Preview
